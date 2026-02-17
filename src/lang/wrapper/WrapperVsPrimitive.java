4d5828425ea91349ba4933c86116ff5e3b840175
im3zero <im3zero@gmail.com>
2026-02-17 20:32:33 +0900

    update

--- src/lang/wrapper/WrapperVsPrimitive.java
+++ src/lang/wrapper/WrapperVsPrimitive.java
@@ -6,15 +6,25 @@ public class WrapperVsPrimitive {
         long startTime, endTime;
 
         //기본형 long 사용
-        long sumPrimitive = 0;
+        long sumPrimitive = 0L;
         startTime = System.currentTimeMillis();
         for (int i = 0; i < iterations; i++) {
-            sumPrimitive += i;
-
+            sumPrimitive += i;  //오토박싱발생
         }
-
         endTime = System.currentTimeMillis();
         System.out.println("sumPrimitive = " + sumPrimitive);
-        System.out.println("기본 자료형 long 실행 시간 = " + (endTime - startTime + "ms"));
+        System.out.println("기본 클래스 long 실행 시간 = " + (endTime - startTime + "ms"));
+
+
+
+        long sumWrapper = 0L;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            sumWrapper += i;  //오토박싱발생
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("sumWrapper = " + sumWrapper);
+        System.out.println("래퍼 클래스 long 실행 시간 = " + (endTime - startTime + "ms"));
+
     }
 }
