7157e8bed9daab52f047bf23ed15bf4c93a7efb0
im3zero <im3zero@gmail.com>
2026-03-06 18:38:41 +0900

    update

---
+++ src/nested/nested/NestedOuter.java
@@ -0,0 +1,24 @@
+package nested.nested;
+
+public class NestedOuter {
+    private static int outClassValue = 3;
+    //같은 클래스 내이므로 private 도 접근 가능
+    private int outInstanceValue = 2;
+
+
+    static class Nested {
+        private int nestedInstanceValue = 1;
+
+        public void print() {
+            //자신의 멤버에 접근
+            System.out.println(nestedInstanceValue);
+
+            //바깥 클래스의 인스턴스 멤버에 접근 불가
+            //System.out.println(outInstanceValue);
+
+            //바깥 클래스의 클래스 멤버에는 접근할 수 있다.
+            //private도 접근 가능
+            System.out.println(NestedOuter.outClassValue);
+        }
+    }
+}
